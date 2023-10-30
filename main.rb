0b9575705598ee403e89b0afbcb883449c2fc3e0
Liam Colburn <colburnw@oregonstate.edu>
2023-10-30 11:23:50 -0700

    Refactor class name and run method name

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 # Exercise 5
 
-class LaunchDiscussionWorkflow
+class DiscussionWorkflow
 
   def initialize(discussion, host, participants_email_string)
     @discussion = discussion
@@ -8,8 +8,15 @@ class LaunchDiscussionWorkflow
     @participants = add_participants_from_email(participants_email_string)
   end
 
+  def add_participants_from_email(participants_email_string)
+    return if participants_email_string.blank?
+    participants_email_string.split.uniq.map do |email_address|
+      @participants << Users.get(email_address)
+    end
+  end
+
   # Expects @participants array to be filled with User objects
-  def run
+  def LaunchDiscussionWorkflow
     return unless valid?
     run_callbacks(:create) do
       ActiveRecord::Base.transaction do
@@ -17,13 +24,6 @@ class LaunchDiscussionWorkflow
         create_discussion_roles!
         @successful = true
       end
-    end
-  end
-
-  def add_participants_from_email(participants_email_string)
-    return if participants_email_string.blank?
-    participants_email_string.split.uniq.map do |email_address|
-      @participants << Users.GetFromEmail(email_address)
     end
   end
 
@@ -36,5 +36,5 @@ discussion = Discussion.new(title: "fake", ...)
 host = User.find(42)
 participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
 
-workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
-workflow.run
+workflow = DiscussionWorkflow.new(discussion, host, participants)
+workflow.LaunchDiscussionWorkflow
