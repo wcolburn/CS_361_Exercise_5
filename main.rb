0717fd2031fa42d1acd0e499dd3cbb0eb24f9bf1
Liam Colburn <colburnw@oregonstate.edu>
2023-10-30 11:21:34 -0700

    Refactor how participant list is generated

--- main.rb
+++ main.rb
@@ -5,8 +5,7 @@ class LaunchDiscussionWorkflow
   def initialize(discussion, host, participants_email_string)
     @discussion = discussion
     @host = host
-    @participants_email_string = participants_email_string
-    @participants = []
+    @participants = add_participants_from_email(participants_email_string)
   end
 
   # Expects @participants array to be filled with User objects
@@ -21,10 +20,10 @@ class LaunchDiscussionWorkflow
     end
   end
 
-  def generate_participant_users_from_email_string
-    return if @participants_email_string.blank?
-    @participants_email_string.split.uniq.map do |email_address|
-      User.create(email: email_address.downcase, password: Devise.friendly_token)
+  def add_participants_from_email(participants_email_string)
+    return if participants_email_string.blank?
+    participants_email_string.split.uniq.map do |email_address|
+      @participants << Users.GetFromEmail(email_address)
     end
   end
 
@@ -38,5 +37,4 @@ host = User.find(42)
 participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
 
 workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
-workflow.generate_participant_users_from_email_string
 workflow.run
